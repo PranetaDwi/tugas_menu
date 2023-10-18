3adb4c1b8eab58d29439ee8b52a6a6d93c7d6819
PranetaDwi <pranetadwiindarti2004@mail.ugm.ac.id>
2023-10-18 20:59:35 +0700

    versi2

--- app/src/main/java/com/neta/tugas_menu/MainActivity.kt
+++ app/src/main/java/com/neta/tugas_menu/MainActivity.kt
@@ -2,10 +2,21 @@ package com.neta.tugas_menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neta.tugas_menu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            viewPager.adapter = TabAdapter(supportFragmentManager)
+            tabLayout.setupWithViewPager(viewPager)
+
+
+        }
+
     }
 }
